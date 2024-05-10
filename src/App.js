0bf169d9be6e0f945512de5733e65b1e95ffbbc5
howardLiuFrogJump <howard.liu@frog-jump.com>
2024-05-10 14:28:43 +0800

    加入前端路由

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Routes , Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+// 0. 於index.js加入BrowserRouter 只要做一次
+//  加入前端路由
+// 1. 準備元件
+// 2. 撰寫Routes
+// 3. 加入連結
 
 function App() {
   return (
@@ -7,7 +16,10 @@ function App() {
       <header className="App-header">
         <Navbar></Navbar>
         <div className="container mt-3">
-          主要內容輸出
+          <Routes>
+            <Route path='/' element={<Home/>}></Route>
+            <Route path='/about' element={<About/>}></Route>
+          </Routes>
         </div>
       </header>
     </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export default function Navbar(){
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
@@ -6,7 +8,10 @@ export default function Navbar(){
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">Home</a>
+              <Link className="nav-link" to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">About</Link>
             </li>
           </ul>
          

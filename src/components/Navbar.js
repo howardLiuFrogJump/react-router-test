af240134fd30666d8e4586996241c0f400d0ecab
howardLiuFrogJump <howard.liu@frog-jump.com>
2024-05-13 12:08:39 +0800

    nav active樣式優化

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function Navbar(){
   return (
@@ -8,13 +8,16 @@ export default function Navbar(){
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link" to="/">Home</Link>
+              <NavLink className="nav-link" style={( {isActive} )=>{
+                return  {color : isActive ? 'red' : ''}
+              }}
+              to="/">Home</NavLink>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/about">關於</Link>
+              <NavLink className="nav-link" to="/about">關於</NavLink>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/album">相簿</Link>
+              <NavLink className="nav-link" to="/album">相簿</NavLink>
             </li>
           </ul>
          
